0866e1399fb384e6ddd946bec8bbc78aafe9ed57
Zeno Dal Maso <zdalmaso@gmail.com>
2018-09-10 13:08:16 +0200

    tarat

--- js/events.js
+++ js/events.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
     var items = 6;
+    links = ["https://www.facebook.com/events/338029743064875/",
+    "https://www.facebook.com/events/411338099027559/",
+    "https://www.facebook.com/events/1574764006112537/",
+    "https://www.facebook.com/events/712945052223127/",
+    "https://www.facebook.com/events/172560280003735/",
+    "#"];
     for(var i = 0; i < items; i++){
         var a = $("<a />", {text: i+1});
         if(i == 0){
@@ -8,7 +14,7 @@ $(document).ready(function () {
         }
         var col = $("<div />", { class : "col" }).append(a);
         $(col).click(function() {
-            
+
             $(active).removeClass("active");
             active = $(this).children().first();
 
@@ -16,7 +22,8 @@ $(document).ready(function () {
 
             var number = $(this).children().first().text();
             $("#imageholder").attr("src","img/events/"+number+".png")
-          });
+            $("#imageholder").parent().attr("onclick","window.open('"+links[number-1]+"','mywindow');");
+        });
         $("#numeri").last().append(col);
     }
 });
